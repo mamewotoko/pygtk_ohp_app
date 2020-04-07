1ea1a443cd510664d2128667abbb35d39968d215
Takashi Masuyama <mamewotoko@gmail.com>
2020-04-08 07:40:02 +0900

    add setup for ubuntu

--- setup.sh
+++ setup.sh
@@ -1,4 +1,14 @@
-#! /bin/sh
+#! /bin/bash
+set -e
 
-brew install pygobject3 gtk+3
-#--with-python@2 
+if [ "$(uname)" = Darwin ]; then
+    brew update
+    brew install pygobject3 gtk+3
+elif [ -f /etc/lsb-release ]; then
+    # debian, ubuntu
+    sudo apt-get update
+    sudo apt-get install -y python3 python3-dev python3-pip libgtk-3-dev
+else
+    echo unsupported platform
+    exit 1
+fi
