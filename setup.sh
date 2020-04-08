3bd02f6da76bd221289e2e30525300ff752bc4cb
Takashi Masuyama <mamewotoko@gmail.com>
2020-04-08 21:47:35 +0900

    pip install wheel

--- setup.sh
+++ setup.sh
@@ -7,12 +7,12 @@ if [ "$(uname)" = Darwin ]; then
     # python3 is installed
     brew link python
     which python
-    pip3 install pycairo
+    pip3 install pycairo wheel
 elif [ -f /etc/lsb-release ]; then
     # debian, ubuntu
     sudo apt-get update
     sudo apt-get install -y python3 python3-dev python3-pip libgtk-3-dev python3-setuptools
-    pip3 install pycairo
+    pip3 install pycairo wheel
 else
     echo unsupported os
     exit 1
