e65467c95a31b99484397da05694f8d722040c07
Takashi Masuyama <mamewotoko@gmail.com>
2020-04-08 21:08:28 +0900

    intall pycairo

--- setup.sh
+++ setup.sh
@@ -8,7 +8,8 @@ if [ "$(uname)" = Darwin ]; then
 elif [ -f /etc/lsb-release ]; then
     # debian, ubuntu
     sudo apt-get update
-    sudo apt-get install -y python3 python3-dev python3-pip libgtk-3-dev pycairo
+    sudo apt-get install -y python3 python3-dev python3-pip libgtk-3-dev
+    pip3 install pycairo
 else
     echo unsupported os
     exit 1
