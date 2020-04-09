d615db56498397d26d1c21d1c855ce4e66573997
Takashi Masuyama <mamewotoko@gmail.com>
2020-04-09 11:17:04 +0900

    output timestamp

--- test.sh
+++ test.sh
@@ -1,11 +1,16 @@
 #! /bin/sh
+set -x
 
 if [ "$(uname)" = Darwin ]; then
     brew install coreutils
+    date "+%Y%m%d-%H%H%S"
     gtimeout --preserve-status 10s ./gtk3_ohp.py
+    date "+%Y%m%d-%H%H%S"
 elif [ -f /etc/lsb-release ]; then
     sudo apt install
+    date "+%Y%m%d-%H%H%S"
     xvfb-run timeout --preserve-status 10s ./gtk3_ohp.py
+    date "+%Y%m%d-%H%H%S"
 else
     echo unsupported platform
     exit 1
