4de0cecb2d735d458f54a0209203ccffcf4b21da
Takashi Masuyama <mamewotoko@gmail.com>
2020-04-09 10:05:49 +0900

    fix typo

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@
 if [ "$(uname)" = Darwin ]; then
     ./gtk3_ohp.py
 elif [ -f /etc/lsb-release ]; then
-    xfvb-run ./gtk3_ohp.py
+    xvfb-run ./gtk3_ohp.py
 else
     echo unsupported platform
     exit 1
