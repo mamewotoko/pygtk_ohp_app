4a63e0c695ac40795c15e80e50c625e70442d836
Takashi Masuyama <mamewotoko@gmail.com>
2020-04-09 15:35:21 +0900

    set font name

--- gtk3_ohp.py
+++ gtk3_ohp.py
@@ -12,6 +12,7 @@ from gi.repository import Gtk
 from gi.repository import Gdk
 from gi.repository import GLib
 import os
+import platform
 import re
 import signal
 
@@ -20,7 +21,8 @@ LINE_WIDTH = 5
 FG_RED = 0
 FG_GREEN = 1
 FG_RED = 0
-
+FONT_SIZE = 24
+FONT_NAME = None
 # Ctrl-z: undo
 # Ctrl-y: redo
 
@@ -143,7 +145,9 @@ class TransparentWindow(Gtk.Window):
                     url = None
                 cr.set_source_rgb(FG_RED, FG_GREEN, FG_BLUE)
                 cr.set_line_width(LINE_WIDTH)
-                cr.set_font_size(20)
+                cr.set_font_size(FONT_SIZE)
+                if FONT_NAME is not None:
+                    cr.select_font_face(FONT_NAME, cairo.FONT_SLANT_NORMAL, cairo.FONT_WEIGHT_NORMAL)
                 (x, y) = shape_info["position"]
 
                 cr.move_to(x, y)
@@ -229,8 +233,21 @@ if __name__ == '__main__':
     parser.add_argument('-g', '--green', type=float, default=1.0)
     parser.add_argument('-b', '--blue', type=float, default=0.0)
     parser.add_argument('-l', '--line-width', type=float, default=4.0)
+    parser.add_argument('-f', '--font', type=str, default=None)
 
     args = parser.parse_args()
+    if args.font is not None:
+        FONT_NAME = args.font
+    else:
+        os_release = platform.system()
+        print(os_release)
+        if os_release == "Darwin":
+            # TODO: if japanese
+            FONT_NAME = "Osaka"
+        elif os_release == "Linux":
+            # ubuntu?
+            FONT_NAME = "takao"
+
     FG_RED = min(1, args.red)
     FG_GREEN = min(1, args.green)
     FG_BLUE = min(1, args.blue)
