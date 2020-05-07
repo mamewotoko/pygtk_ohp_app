ac91539f88bf9684a7bb9eb5392a1b2b355fdc65
Takashi Masuyama <mamewotoko@gmail.com>
2020-05-07 21:59:57 +0900

    modify setup command of travis

--- setup.sh
+++ setup.sh
@@ -20,11 +20,18 @@ elif [ -f /etc/lsb-release ]; then
     sudo apt-get install -y python3 python3-dev python3-pip libgtk-3-dev python3-setuptools xvfb pkg-config
     python3 -m pip install -r requirements.txt
 
-elif [[ "$UNAME" == "MINGW64_NT"* ]] || [[ "$UNAME" == "MSYS_NT"* ]]; then
-    # mingw64
+elif [[ "$UNAME" == "MINGW64_NT"* ]]; then
+    # msys2 on PC
     pacman -Syu --noconfirm
     pacman -Sy --noconfirm mingw-w64-x86_64-gtk3 mingw-w64-x86_64-python3 mingw-w64-x86_64-python3-gobject 
     pacman -Sy --noconfirm mingw-w64-x86_64-python3-setuptools mingw-w64-x86_64-python3-pip
+    python3 -m pip install -r requirements.txt
+
+elif [[ "$UNAME" == "MSYS_NT"* ]]; then
+    # travis
+    $msys2 pacman -Syu --noconfirm
+    $msys2 pacman -Sy --noconfirm mingw-w64-x86_64-gtk3 mingw-w64-x86_64-python3 mingw-w64-x86_64-python3-gobject 
+    $msys2 pacman -Sy --noconfirm mingw-w64-x86_64-python3-setuptools mingw-w64-x86_64-python3-pip
     python3 -m pip install -r requirements.txt
 
 else
