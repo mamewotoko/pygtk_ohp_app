c4db37f1045fbd6ddc23a41afede07e6d95b8dc2
Takashi Masuyama <mamewotoko@gmail.com>
2020-05-08 06:45:26 +0900

    add test on windows

--- test.sh
+++ test.sh
@@ -1,14 +1,23 @@
 #! /bin/sh
 set -x
 
-if [ "$(uname)" = Darwin ]; then
+UNAME=$(uname)
+
+if [ "$UNAME" = Darwin ]; then
     brew install coreutils
     date "+%Y%m%d-%H%H%S"
     gtimeout --preserve-status 10s ./gtk3_ohp.py
     date "+%Y%m%d-%H%H%S"
+
 elif [ -f /etc/lsb-release ]; then
     date "+%Y%m%d-%H%H%S"
     xvfb-run timeout --preserve-status 10s ./gtk3_ohp.py
+    date "+%Y%m%d-%H%H%S"
+
+elif [[ "$UNAME" == "MINGW64_NT"* ]] || [[ "$UNAME" == "MSYS_NT"* ]]; then
+
+    date "+%Y%m%d-%H%H%S"
+    timeout --preserve-status 10s ./gtk3_ohp.py
     date "+%Y%m%d-%H%H%S"
 else
     echo unsupported platform
