2af70040cf8727b12222a027cbaf321e99ab5bf2
Takashi Masuyama <mamewotoko@gmail.com>
2020-05-17 20:08:52 +0900

    modify setup script of mac

--- setup.sh
+++ setup.sh
@@ -6,26 +6,24 @@ UNAME="$(uname)"
 # TODO: venv?
 
 if [ "$UNAME" = Darwin ]; then
-    brew update
-    which python
-    python --version
-    brew link --overwrite python
-    brew install pygobject3
+#    brew update
+#    brew install pygobject3
     brew install gtk+3 pkg-config
-    python3 -m pip install -r requirements.txt
+    python3 -m pip install --user -r requirements.txt
+    python3 -m pip install --user pygobject
 
 elif [ -f /etc/lsb-release ]; then
     # debian, ubuntu
     sudo apt-get update
     sudo apt-get install -y python3 python3-dev python3-pip libgtk-3-dev python3-setuptools xvfb pkg-config
-    python3 -m pip install -r requirements.txt
+    python3 -m pip install --user -r requirements.txt
 
 elif [ -f /etc/redhat-release ] && grep "release 7" /etc/redhat-release; then
     # redhat, centos7
     sudo yum install -y gcc gobject-introspection-devel cairo-devel \
          pkg-config python3-devel gtk3 python3-pip pygobject3-devel cairo-gobject-devel
-    sudo python3 -m pip install PyGObject pycairo
-    sudo python3 -m pip install -r requirements.txt
+    python3 -m pip install --user  PyGObject pycairo
+    python3 -m pip install --user -r requirements.txt
     # install x11
     # http://morrey22.hatenablog.com/entry/2013/04/14/212837
     # sudo yum -y groups install "GNOME Desktop" 
