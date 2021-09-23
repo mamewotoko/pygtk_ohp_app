16305ca3ee155e1d61f4f734bc3e4b3ddad93426
Takashi Masuyama <mamewotoko@gmail.com>
2021-09-23 12:39:35 +0900

    remove requirements.txt

--- setup.sh
+++ setup.sh
@@ -14,7 +14,7 @@ if [ "$UNAME" = Darwin ]; then
     #brew upgrade
     brew install pygobject3
     brew install gtk+3 python3
-    python3 -m pip install --user -r requirements.txt
+    python3 -m pip install --user .
     python3 -m pip install --user pygobject
 
 elif [ -f /etc/lsb-release ] || [ -f /etc/debian_version ]; then
@@ -22,14 +22,14 @@ elif [ -f /etc/lsb-release ] || [ -f /etc/debian_version ]; then
     sudo apt-get update
     sudo apt-get install -y python3 python3-dev python3-pip libgtk-3-dev \
          python3-setuptools xvfb pkg-config python3-gi-cairo
-    python3 -m pip install --user -r requirements.txt
+    python3 -m pip install --user -r .
 
 elif [ -f /etc/redhat-release ] && grep "release 7" /etc/redhat-release; then
     # redhat, centos7
     sudo yum install -y gcc gobject-introspection-devel cairo-devel \
          pkg-config python3-devel gtk3 python3-pip pygobject3-devel cairo-gobject-devel
     python3 -m pip install --user PyGObject pycairo
-    python3 -m pip install --user -r requirements.txt
+    python3 -m pip install --user -r .
     # install x11
     # http://morrey22.hatenablog.com/entry/2013/04/14/212837
     # sudo yum -y groups install "GNOME Desktop"
@@ -53,14 +53,14 @@ elif [[ "$UNAME" == "MINGW64_NT"* ]]; then
     pacman -Syu --noconfirm || true
     pacman -Sy --noconfirm mingw-w64-x86_64-gtk3 mingw-w64-x86_64-python3 mingw-w64-x86_64-python3-gobject
     pacman -Sy --noconfirm mingw-w64-x86_64-python3-setuptools mingw-w64-x86_64-python3-pip
-    python3 -m pip install -r requirements.txt
+    python3 -m pip install .
 
 elif [[ "$UNAME" == "MSYS_NT"* ]]; then
     # travis
     $msys2 pacman -Syu --noconfirm
     $msys2 pacman -Sy --noconfirm mingw-w64-x86_64-gtk3 mingw-w64-x86_64-python3 mingw-w64-x86_64-python3-gobject
     $msys2 pacman -Sy --noconfirm mingw-w64-x86_64-python3-setuptools mingw-w64-x86_64-python3-pip
-    python3 -m pip install -r requirements.txt
+    python3 -m pip install .
 
 else
     echo unsupported os
